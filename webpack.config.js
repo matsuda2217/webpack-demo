f4f6faf1e66aada79d247ed257af4ddf1d5dec0e
Tai Le <tai.le-cr@rockyou.com>
2018-10-10 17:36:20 +0700

    aaaaa

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,50 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo"
-    })
-  ]
+const merge = require("webpack-merge");
+const parts = require('./webpack.part');
+
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo"
+      }),
+    ],
+    
+  },
+  parts.loadCSS(),
+]);
+const productionConfig = merge([])
+
+const developmentConfig = merge([
+  parts.devServer({
+    host: process.env.HOST,
+    port: process.env.PORT
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  } else {
+    return merge(commonConfig, developmentConfig, { mode })
+  }
 }
+// module.exports = {
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: "Webpack demo"
+//     })
+//   ],
+//   devServer: {
+//     // display errors
+//     stats: 'errors-only',
+//     overlay: {
+//       warnings: true,
+//       errors: true
+//     },
+//     host: process.env.HOST,
+//     port: process.env.PORT,
+//     open: true
+//   }
+// }
+
